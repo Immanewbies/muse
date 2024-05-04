286dd61a1e600ae1851cba0517d42a8f510efa74
root <immanewbies@gmail.com>
2024-05-04 20:57:52 +0700

    Final prod#1 Test#28 Deploy EC2

--- server/test/server/server.test.js
+++ server/test/server/server.test.js
@@ -1,18 +1,26 @@
-import { expect, use } from 'chai';
-import chaiHttp from 'chai-http'
+import { expect } from 'chai';
+import request from 'supertest';
 import app from '../../server.js'; // Assuming your server.js file exports the app
-const chai = use(chaiHttp);
-chai.should();
 
 describe('Server Tests', () => {
+  let server;
+
+  before(() => {
+    server = app.listen(0); // Start the server on a dynamic port (0) for testing
+  });
+
+  after(() => {
+    server.close(); // Close the server after testing
+  });
+
   it('should return status 200 for GET / with Error UR not authenticated', async () => {
-    const res = await chai.request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.status).to.equal(200);
     expect(res.body).to.have.property('Error', 'UR not authenticated');
   });
 
   it('should return status 200 and success message for POST /register', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/register')
       .send({
         username: 'testuser',
@@ -24,7 +32,7 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 and success message for POST /login', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/login')
       .send({
         username: 'fresh2',
@@ -35,7 +43,7 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 for GET /', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/login')
       .send({
         username: 'fresh2',
@@ -49,13 +57,13 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 and success message for GET /logout', async () => {
-    const res = await chai.request(app).get('/logout');
+    const res = await request(server).get('/logout');
     expect(res.status).to.equal(200);
     expect(res.body).to.have.property('Status', 'Success');
   });
 
   it('should return status 200 and an array for POST /chord/findchord', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/chord/findchord')
       .send({
         chord_note: 'C',
@@ -66,7 +74,7 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 and an array for POST /scale/findscale', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/scale/findscale')
       .send({
         scale_note: 'C',
@@ -77,7 +85,7 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 and an array for POST /eartrain/question', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/eartrain/question')
       .send({
         difficulty_name: 'Easy',
@@ -88,19 +96,19 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 and an array for GET /eartrain/note', async () => {
-    const res = await chai.request(app).get('/eartrain/note');
+    const res = await request(server).get('/eartrain/note');
     expect(res.status).to.equal(200);
     expect(res.body).to.be.an('array');
   });
 
   it('should return status 200 and an array for GET /eartrain/chord', async () => {
-    const res = await chai.request(app).get('/eartrain/chord');
+    const res = await request(server).get('/eartrain/chord');
     expect(res.status).to.equal(200);
     expect(res.body).to.be.an('array');
   });
 
   it('should return status 200 and an array for POST /quiz/question', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/quiz/question')
       .send({
         difficulty_name: 'Easy',
@@ -111,7 +119,7 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 and success message for POST /user/score', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/user/score')
       .send({
         profile_name: 'Test User',
@@ -124,7 +132,7 @@ describe('Server Tests', () => {
   });
 
   it('should return status 200 and an array for POST /user/getscore', async () => {
-    const res = await chai.request(app)
+    const res = await request(server)
       .post('/user/getscore')
       .send({
         profile_name: 'Test User',
